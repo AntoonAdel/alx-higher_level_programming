19f3b35c988e88a05f258091265b8a49cf7227b9
AntoonAdel <antonadel359@gmail.com>
2023-12-04 05:12:43 +0200

    upload the task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    count = len(argv) - 1
-    if count == 0:
-        print(f"{count:d} arguments.")
-    elif count == 1:
-        print(f"{count:d} arguments.")
+    counter = len(argv) - 1
+    if counter == 0:
+        print("{:d} arguments.".format(counter))
+    elif counter == 1:
+        print("{:d} argument:".format(counter))
     else:
-        print(f"{count:d} arguments.")
+        print("{:d} arguments:".format(counter))
     for a in range(0, len(argv)):
         if a > 0:
-            print(f"{a:d}: {argv[a]:s}")
+            print("{:d}: {:s}".format(a, argv[a]))
