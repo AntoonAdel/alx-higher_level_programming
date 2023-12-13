39db1f29767fcc028dbffbc7b583569b1718fd17
AntoonAdel <antonadel359@gmail.com>
2023-12-13 15:34:38 +0200

    solve task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    no_c_list = list(my_string)
-    [no_c_list.remove(cC) for cC in no_c_list if cC == 'c' or cC == 'C']
-    return("".join(no_c_list))
+    clist = list(my_string)
+    [clist.remove(a) for a in clist if a == 'c' or a == 'C']
+    return("".join(clist))
