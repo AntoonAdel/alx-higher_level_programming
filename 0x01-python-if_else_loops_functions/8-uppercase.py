2ce8940bbcbc80dec69983bc965d0d964e235db3
AntoonAdel <antonadel359@gmail.com>
2024-04-05 19:15:53 +0200

    Done task 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for arr in range(len(str)):
+        char = ord(str[arr])
+        if (char >= 97) and (char <= 122):
+            char -= 32
+        print("{}".format(chr(char)), end="")
+    print("\n")
