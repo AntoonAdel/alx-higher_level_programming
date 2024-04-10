85d3aa4d8bc0d1559a1a5e84642eb07bec8f4520
AntoonAdel <antonadel359@gmail.com>
2024-04-10 18:40:22 +0200

    Done task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Class that creates an object Rectangle
+"""
+
+
+class Rectangle:
+    """ Class that creates an object Rectangle """
+    def __init__(self, width=0, height=0):
+        """ Constructor """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Width Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Width Setter """
+        if not isinstance(value, int):
+            err = "width must be an integer"
+            raise TypeError(err)
+        if (value < 0):
+            err = "width must be >= 0"
+            raise ValueError(err)
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height Getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height Setter """
+        if not isinstance(value, int):
+            err = "height must be an integer"
+            raise TypeError(err)
+        if (value < 0):
+            err = "height must be >= 0"
+            raise ValueError(err)
+        self.__height = value
+
+    def area(self):
+        """ Method for area calculation """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ Method for perimeter calculation """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """ Returns the Rectangle for printing """
+        Rec_str = ""
+        if self.width == 0 or self.height == 0:
+            return (Rec_str)
+        else:
+            for test in range(self.height):
+                Rec_str += "{}".format('#'*self.__width)
+                if test != (self.height - 1):
+                    Rec_str += "\n"
+        return Rec_str
