3c5b019ff8042a0b2c586c4739f12b48b4e9204c
AntoonAdel <antonadel359@gmail.com>
2024-04-10 17:40:04 +0200

    Done task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class Square that defines methods and attributes for a square object
+"""
+
+
+class Square:
+    """
+    Class Square that defines methods and attributes for a square object
+    """
+
+    def __init__(self, size=0):
+        """ Class Constructor"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
