5d104ee53313bb2e319acb5160c1a8758890546e
AntoonAdel <antonadel359@gmail.com>
2024-04-11 02:11:14 +0200

    Done task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+returns True if the object is exactly an instance of the specified class
+otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of the specified class
+    otherwise False
+    """
+    return isinstance(obj, a_class) == a_class
