aae4bbbc98eb7854880b3a9b0d021c790d76a91f
AntoonAdel <antonadel359@gmail.com>
2024-04-11 02:28:56 +0200

    Done task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry Class raising an exception """
+
+class BaseGeometry:
+    """ Geometry Class """
+
+    def area(self):
+        """ Raise an exception """
+        raise Exception("area() is not implemented")
