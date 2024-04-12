a7b714b4d55056482b7102df9e57305c40cc8b2a
AntoonAdel <antonadel359@gmail.com>
2024-04-12 15:38:50 +0200

    Done All TASKS

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Setting the width of the rectangle """
-        if isinstance(value) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -38,7 +38,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ Setting the height of the rectangle """
-        if isinstance(value) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -52,7 +52,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Setting the x coordinate of the rectangle """
-        if isinstance(value) is not int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -66,7 +66,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Setting the y coordinate of the rectangle """
-        if isinstance(value) is not int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -93,7 +93,7 @@ class Rectangle(Base):
         if args:
             for arg in range(len(args)):
                 if arg == 0:
-                    if isinstance(args[arg]) is not int:
+                    if type(args[arg]) is not int:
                         raise TypeError("id must be an integer")
                     self.id = args[arg]
                 elif arg == 1:
@@ -107,7 +107,7 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 if key == "id":
-                    if isinstance(value) is not int:
+                    if type(value) is not int:
                         raise TypeError("id must be an integer")
                     self.id = value
                 elif key == "width":
