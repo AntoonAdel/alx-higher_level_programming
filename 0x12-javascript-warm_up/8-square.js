8c4f5222f07d505908b9a03ddd054234fb337d74
AntoonAdel <antonadel359@gmail.com>
2024-04-26 02:38:41 +0300

    Done task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const square = parseInt(process.argv[2]);
+let size = 0;
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+    for (size = 0; size < square; size++) {
+            console.log('X'.repeat(square));
+    }
+}
